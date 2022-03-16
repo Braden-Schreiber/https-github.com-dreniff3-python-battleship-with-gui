32a68e6e554ea47eb025109596b311b8956a05bc
Don Reniff <donaldreniff@gmail.com>
2022-03-15 18:13:59 -0700

    declare classes

--- ship_game_classes.py
+++ ship_game_classes.py
@@ -2,3 +2,13 @@
 # Github username: dreniff3
 # Description: This module contains the classes necessary for instantiating a simulated game of Battleship.
 
+class Ship:
+    pass
+
+
+class Player:
+    pass
+
+
+class ShipGame:
+    pass
