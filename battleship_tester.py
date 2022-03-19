d7c05371f6d3e2d7a06663b027088512713669a5
Don Reniff <donaldreniff@gmail.com>
2022-03-19 11:49:47 -0700

    add method to ShipGame class for counting remaining ships and test to test file for num ships player has placed

--- battleship_tester.py
+++ battleship_tester.py
@@ -14,3 +14,9 @@ class TestShipGame(unittest.TestCase):
         s.place_ship("first", 'destroyer', 'F8', 'R')
         self.assertEqual('destroyer' in s._player_1.get_ships(), True)
         self.assertEqual(s._player_1.get_ships()['destroyer'].get_location(), [(5, 7), (5, 8)])
+
+    def test_place_ship_2(self):
+        s = ShipGame()
+        s.place_ship("first", 'destroyer', 'F8', 'R')
+        s.place_ship("first", 'carrier', 'J9', 'R')
+        self.assertEqual(s.get_num_ships_remaining("first"), 1)
