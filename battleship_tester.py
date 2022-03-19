59fd602f75ded56cd92adb572bcd22913656bc50
Don Reniff <donaldreniff@gmail.com>
2022-03-19 11:43:16 -0700

    add tests for instantiating ShipGame object and for placing a ship as player 1

--- battleship_tester.py
+++ battleship_tester.py
@@ -4,4 +4,13 @@ from ship_game_classes import ShipGame
 
 class TestShipGame(unittest.TestCase):
     """ Contains unit tests for ship_game_classes module. """
-    pass
+
+    def test_game_instantiation(self):
+        s = ShipGame()
+        self.assertIsInstance(s, ShipGame)
+
+    def test_place_ship_1(self):
+        s = ShipGame()
+        s.place_ship("first", 'destroyer', 'F8', 'R')
+        self.assertEqual('destroyer' in s._player_1.get_ships(), True)
+        self.assertEqual(s._player_1.get_ships()['destroyer'].get_location(), [(5, 7), (5, 8)])
