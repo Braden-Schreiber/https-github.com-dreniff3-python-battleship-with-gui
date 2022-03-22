810fdb0ea23b4f9425a8f268f3bae426ded8784d
Don Reniff <donaldreniff@gmail.com>
2022-03-22 15:47:07 -0700

    add radiobuttons for which player is making move

--- battleship.py
+++ battleship.py
@@ -21,6 +21,12 @@ frame3.grid(row=1, column=0, padx=30, pady=20)
 frame4 = LabelFrame(root, text="Ships Remaining", padx=15, pady=15)
 frame4.grid(row=1, column=1, padx=10, pady=10)
 
+# which player is making move
+player = StringVar()
+player.set('first')
+Radiobutton(frame3, text="Player1", variable=player, value='first').grid(row=0, column=0)
+Radiobutton(frame3, text="Player2", variable=player, value='second').grid(row=0, column=1)
+
 # game board buttons
 
 # Player1 board
